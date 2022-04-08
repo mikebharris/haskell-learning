a4fe716cd0cff02c66848b46263787ce1e9b1d6f
Mike Harris <m.harris@elsevier.com>
2022-04-08 14:34:16 +0100

    End of C14

--- app/Chapter14.hs
+++ app/Chapter14.hs
@@ -35,7 +35,7 @@ module Chapter14 where
     --     (<=) MA NH = True
     --     (<=) _ _ = False
 
-    data Name = Name (String, String) deriving (Show, Eq)
+    newtype Name = Name (String, String) deriving (Show, Eq)
     instance Ord Name where
         compare (Name (f1,l1)) (Name (f2,l2)) = compare (l1,f1) (l2,f2)
 
