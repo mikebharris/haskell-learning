9c2e95c397802cff8bffa7d0e931cf1b65d1fba2
Mike Harris <m.harris@elsevier.com>
2023-04-23 11:26:54 +0100

    Done Chapter 3

--- greatgood/Chap3.hs
+++ greatgood/Chap3.hs
@@ -31,8 +31,8 @@ anorexiaWarning = "Anorexic!"
 fattyDetector :: Double -> Double -> String
 fattyDetector weightKg heightM
   | bmi <= skinny = anorexiaWarning
-  | bmi  <= normal = "Hench!"
-  | bmi  <= fat = "Sort it out fatso!"
+  | bmi <= normal = "Hench!"
+  | bmi <= fat = "Sort it out fatso!"
   | otherwise = "Go see a doctor fatarse!"
   where bmi = weightKg / heightM ^ 2
         skinny = 18.5
@@ -48,7 +48,7 @@ initials2 :: String -> String -> String
 initials2 (f:_) (l:_) = [f] ++ ". " ++ [l] ++ "."
 
 calcBmis :: [(Double, Double)] -> [Double]
-calcBmis xs = [bmi w h | (w, h) <-xs]
+calcBmis xs = [bmi w h | (w, h) <-xs, bmi w h > 25.0]
   where bmi weight height = weight / height ^ 2
 
 cylinder :: Double -> Double -> Double
@@ -57,4 +57,26 @@ cylinder r h =
       topArea = pi * r ^ 2
   in sideArea + 2 * topArea
 
-[ x * x | x <- [5, 3, 2]] == let square x = x * x in [square 5, square 3, square 2] -- True
+--[ x * x | x <- [5, 3, 2]] == let square x = x * x in [square 5, square 3, square 2] -- True
+
+calcBmisLet :: [(Double, Double)] -> [Double]
+calcBmisLet xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
+
+head' :: [a] -> a
+head' xs = case xs of []    -> error "You're a putz!"
+                      (x:_) -> x
+
+describeList :: [a] -> String
+describeList ls = "blah " ++ case ls of []  -> "empty"
+                                        [x] -> "singleton"
+                                        xs  -> "longer"
+
+--fattyDetector2 :: Double -> Double -> String
+--fattyDetector2 weightKg heightM = case bmi of skinny -> anorexiaWarning
+--                                              normal -> "Hench!"
+--                                              fat    -> "Sort it out fatso!"
+--                                              _      -> "Go see a doctor fatarse!"
+--  where bmi = weightKg / heightM ^ 2
+--        skinny = 18.5
+--        normal = 25.0
+--        fat = 30.0
