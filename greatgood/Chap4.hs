ed22e601af9d41d30edea6a50ca5075c10800502
Mike Harris <m.harris@elsevier.com>
2023-05-08 17:58:34 +1000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:greatgood/Chap4.hs

--- greatgood/Chap4.hs
+++ greatgood/Chap4.hs
@@ -23,3 +23,30 @@ replicateg :: Int -> a -> [a]
 replicateg n x
   | n <= 0 = []
   | otherwise = x : replicateg (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n (x:xs)
+  | n <= 0 = []
+  | otherwise = x : take' (n-1) xs
+take' _ [] = []
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+
+
