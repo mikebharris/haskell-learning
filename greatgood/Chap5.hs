49eb8e65ac8b94284f267871fb17be210f7d32f7
naomi <naomirosenberguk@gmail.com>
2023-05-10 09:00:16 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:greatgood/Chap5.hs

--- greatgood/Chap5.hs
+++ greatgood/Chap5.hs
@@ -7,6 +7,8 @@
 
 -- is a function that takes an Int and returns a function that takes an Int
 -- and returns a function that takes an Int and returns an Int
+-- Here the brackets are not necessary because type definitions are right-associative
+-- and expressions are left-associative
 multThree :: Int -> (Int -> (Int -> Int))
 multThree x y z = x * y * z
 r1 = ((multThree 3) 5) 9
@@ -19,5 +21,17 @@ compWithHundred :: Int -> Ordering
 --compWithHundred x = compare 100 x
 -- is equivalent to, the x's are on both sides and can be cancelled out
 compWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+divideTenBy :: (Floating a) => a -> a
+divideTenBy = (10/)
+
+isUpperAlpha :: Char -> Bool
+isUpperAlpha = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f a = f (f a)
 
 
