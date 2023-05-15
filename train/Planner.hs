f1e3d885def6872a76506a891bbab402877286fb
Mike Harris <m.harris@elsevier.com>
2023-05-15 17:06:21 +1000

    Mike

--- train/Planner.hs
+++ train/Planner.hs
@@ -1,8 +1,15 @@
 module Planner
   where
+    import Data.Time
   
     duration :: [[String]] -> String -> String -> String -> Int
-    duration timetable passengerArrivalTime startStationName endStationName = 150
-    
+    duration timetable passengerArrivalTime startStationName endStationName = endStationTime - startStationTime
+      where endStationTime = timetable !! train !! endStationIdx
+            endStationIdx = length (takeWhile (/= endStationName) (stations))
+            startStationTime = timetable !! train !! startStationIdx
+            startStationIdx = length (takeWhile (/= startStationName) (stations))
+            stations = timetable !! 0
+            train = [a | a <- [1..3]]
+
     fastestTrain :: [[String]] -> String -> String -> String
     fastestTrain timetable startStationName endStationName = "1357"
